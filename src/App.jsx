caa8574921ed095d6f85e52f75d7cd1ce2a5bcba
Jaeeun <jaeeun9862@gmail.com>
2023-11-01 10:54:59 +0900

    firebase로 배포시도

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import "./App.scss";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./components/Root";
-import Home from "./components/Home";
-import List from "./components/List";
-import ListInfo from "./components/ListInfo";
-import AddForm from "./components/AddForm";
+import React from 'react';
+import './App.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './components/Root';
+import Home from './components/Home';
+import List from './components/List';
+import ListInfo from './components/ListInfo';
+import AddForm from './components/AddForm';
 
 const App = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <Root />,
-      errorElement: "Not Found",
+      errorElement: 'Not Found',
       children: [
         { index: true, element: <Home /> },
-        { path: "/list", element: <List /> },
-        { path: "/list/:infoId", element: <ListInfo /> },
-        { path: "/addform", element: <AddForm /> },
+        { path: '/list', element: <List /> },
+        { path: '/list/:infoId', element: <ListInfo /> },
+        { path: '/addform', element: <AddForm /> },
       ],
     },
   ]);
 
   return (
-    <div className="App">
+    <div className='App'>
       <RouterProvider router={router} />
     </div>
   );
